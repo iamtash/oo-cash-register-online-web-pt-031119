17aad4b3847d1d0dedbd144e7ca9fe6f7685c0f6
Natasha Chernis <natashachernis@gmail.com>
2019-04-06 19:04:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,13 +1,21 @@
 class CashRegister
+  attr_reader :total :employee_discount :items
+  
   def initialize(employee_discount = nil)
     @total = 0
     @employee_discount = employee_discount
+    @items = []
   end
 
   def total
     @total
   end
 
-  def add_item(title, price)
+  def add_item(title, price, quantity = 1)
   end
+
+  def apply_discount
+  end
+
+  
 end
