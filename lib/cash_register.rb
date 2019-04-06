c2ab0e695158da3215133d186fa3a23d7b3b165e
Natasha Chernis <natashachernis@gmail.com>
2019-04-06 19:24:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,8 +8,8 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
-    @items << title
-    @total += price * quantity
+    self.items << title
+    self.total += price * quantity
   end
 
   def apply_discount
