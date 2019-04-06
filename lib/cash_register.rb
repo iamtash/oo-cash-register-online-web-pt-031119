7eb2e0e7f79ecac5c39a798494449c0503496d7c
Natasha Chernis <natashachernis@gmail.com>
2019-04-06 19:41:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,7 +4,7 @@ class CashRegister
 
   def initialize(employee_discount = nil)
     @total = 0
-    @employee_discount = employee_discount
+    @employee_discount = employee_discount if employee_discount
     @items = []
   end
 
