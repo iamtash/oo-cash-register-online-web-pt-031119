20883f2e902aa61996879d4a6fd26c536465f6ef
Natasha Chernis <natashachernis@gmail.com>
2019-04-06 19:52:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,6 +16,7 @@ class CashRegister
   def apply_discount
     if self.discount > 0
       self.total *= self.discount
+      "After the discount, the total comes to $#{self.total}."
     end
   end
 
